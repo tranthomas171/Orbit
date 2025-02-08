1050a51727df798c8f6b7d625dbe2b009f65de26
Ethan W <ethan.wanq@gmail.com>
2025-02-08 05:47:05 -0500

    prepare session dashboardl ogin

--- extension/popup.js
+++ extension/popup.js
@@ -1,3 +1,9 @@
-document.getElementById('dashboard').addEventListener('click', () => {
-    chrome.tabs.create({ url: 'http://localhost:3000' }); // Change this to your dashboard URL
+document.getElementById('dashboard').addEventListener('click', async () => {
+    let token = await chrome.storage.local.get('access_token');
+    if(token) {
+      token = "?t="+token
+    } else {
+      token = ""
+    }
+    chrome.tabs.create({ url: 'http://localhost:5173' + token }); // Change this to your dashboard URL
   });
